37752267fe68f0d6858f38c73e0dc5fd4f0cc075
Cyril Robert <Cyril Robert crobert@inverse.ca>
2010-04-01 15:53:11 -0400

    Correction bug des liens de page

--- auf_savoirs_en_partage_django/media/js/pagination.js
+++ auf_savoirs_en_partage_django/media/js/pagination.js
@@ -1,5 +1,5 @@
 function urlencode(str) {
-    return escape(str).replace(/\+/g,'%2B').replace(
+    return escape(unescape(str)).replace(/\+/g,'%2B').replace(
                  /%20/g, '+').replace(/\*/g, '%2A').replace(
                  /\//g, '%2F').replace(/@/g, '%40');
 }

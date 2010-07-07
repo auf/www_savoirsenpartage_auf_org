868215e982c5243a8e9523a46e569499c321355a
Cyril Robert <Cyril Robert cyrilrbt@gmail.com>
2010-07-07 10:16:48 -0400

    Utilisation davical

--- auf_savoirs_en_partage_django/savoirs/__init__.py
+++ auf_savoirs_en_partage_django/savoirs/__init__.py
@@ -5,6 +5,8 @@ configuration = {
     'resultats_par_page': 8, # pas changeable a cause de google
     'accueil_evenement': 10,
     'engin_recherche': 'sep',
-    'calendrier': 'http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/personal/',
-    'calendrier_publique': 'http://sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/personal/',
+    #'calendrier': 'http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/personal/',
+    #'calendrier_publique': 'http://sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/personal/',
+    'calendrier': 'http://davin:davin@cal.ro.auf/caldav.php/davin/test/',
+    'calendrier_publique': 'http://cal.ro.auf/caldav.php/davin/test/',
 }

f3bbc2b422ac1366eff275265ec59bf3f8ff4175
davin baragiotta <davin.baragiotta@u-db.(none)>
2010-07-09 11:54:15 -0400

    agenda - modif config acces serveur

--- auf_savoirs_en_partage_django/savoirs/__init__.py
+++ auf_savoirs_en_partage_django/savoirs/__init__.py
@@ -7,6 +7,6 @@ configuration = {
     'engin_recherche': 'sep',
     #'calendrier': 'http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/personal/',
     #'calendrier_publique': 'http://sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/personal/',
-    'calendrier': 'http://davin:davin@cal.ro.auf/caldav.php/davin/test/',
-    'calendrier_publique': 'http://cal.ro.auf/caldav.php/davin/test/',
+    'calendrier': 'http://davin:davin@cal.ro.auf.org/caldav.php/davin/test/',
+    'calendrier_publique': 'http://cal.ro.auf.org/caldav.php/davin/test/',
 }

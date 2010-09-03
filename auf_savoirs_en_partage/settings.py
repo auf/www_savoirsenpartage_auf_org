7a355faa06cac49fcb349337a5146cd44c34ad81
Ali Jetha <ali@U-AJ.(none)>
2010-09-03 16:33:59 -0400

    pagination/sorting dans repertoire

--- auf_savoirs_en_partage/settings.py
+++ auf_savoirs_en_partage/settings.py
@@ -29,12 +29,30 @@ ADMIN_MEDIA_PREFIX = '/admin_media/'
 # Don't share this with anybody.
 SECRET_KEY = 'k5m_cyht&ipi!nbxng9-5zc!4_q84opflhsn#d3%^3=9gm44tt'
 
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+)
+
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+    'django_sorting.middleware.SortingMiddleware',
     'django.middleware.doc.XViewMiddleware',
 )
 
@@ -46,6 +64,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.admin',
+    'pagination',
+    'django_sorting',
     'django_roa',
     'timezones',
     'savoirs',

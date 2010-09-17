03e081cb79df194bac2b5f4cc00bff6519f67a49
Ali Jetha <ali@U-AJ.(none)>
2010-09-17 15:51:46 -0400

    Modeles sitotheque

--- auf_savoirs_en_partage/sites/models.py
+++ auf_savoirs_en_partage/sites/models.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django.db import models
+from datamaster_modeles.models import *
+from savoirs.models import Discipline
 
 TYPE_SITE_CHOICES = (
     ('RV', 'Revue en ligne'), 
@@ -13,22 +15,24 @@ TYPE_SITE_CHOICES = (
 class Site(models.Model):
     """Fiche d'info d'un site web"""
     url = models.URLField(verify_exists=True)   # dc:identifier (dc:source?)
-    titre = models.CharField(max_length=255, null=False, blank=False, verbose_name = 'Titre')   # dc.title
+    titre = models.CharField(max_length=255, verbose_name='Titre')   # dc.title
     description = models.TextField()
-    editeur = models.CharField(max_length=255, verbose_name = 'Éditeur')    # dc.publisher : organisation resp
-    auteur = models.CharField(max_length=255, verbose_name = 'Auteur')  # dc.creator : nom, prénom
-    auf_partenaire = models.BooleanField()    # dc.contributor
+    editeur = models.CharField(max_length=255, verbose_name='Éditeur')    # dc.publisher : organisation resp
+    auteur = models.CharField(max_length=255, verbose_name='Auteur')  # dc.creator : nom, prénom
+    
+    #auf_partenaire = models.BooleanField()    # dc.contributor
+    
     date_publication = models.DateField()      # dc.date : date de publication
     type = models.CharField(max_length=2, null=False, blank=False, choices=TYPE_SITE_CHOICES,
                               verbose_name = 'Type de site')    # dc.type
-    # thematiques   # fk Thematiques
-    # disciplines   # fk Disciplines    # dc:subject
-    # mots_cles     # dc:subject    # indexation libre
+    discipline = models.ManyToManyField(Discipline, blank=True)
+    thematique = models.ManyToManyField(Thematique, blank=True)
+    
+    mots_cles =  models.TextField(verbose_name='Mots-clés')    # dc:subject    # indexation libre
 
     # source    # dc:source (dc:relation?)
-    # pays  # fk Pays      # dc:coverage   # pays de production
-    # droits    # dc:rights
+    pays = models.ForeignKey(Pays, null = True, db_column='pays', to_field='code', verbose_name = 'Pays')
     
     # meta
     actif = models.BooleanField()
-    # date_maj
+    date_maj = models.DateField(auto_now=True)

ca678c8eeb8247777f9dc33c4f24cc4d2f960743
Eric Mc Sween <eric.mcsween@gmail.com>
2010-10-28 16:49:51 -0400

    Filtrer les doublons dans la recherche par mots-clés de ressources.

--- auf_savoirs_en_partage/savoirs/models.py
+++ auf_savoirs_en_partage/savoirs/models.py
@@ -245,7 +245,7 @@ class RecordQuerySet(models.query.QuerySet):
                            Q(creator__icontains=word) | Q(contributor__icontains=word) |
                            Q(subject__icontains=word) | Q(disciplines__nom__icontains=word) |
                            Q(regions__nom__icontains=word) | Q(pays__nom__icontains=word) |
-                           Q(pays__region__nom__icontains=word))
+                           Q(pays__region__nom__icontains=word)).distinct()
 
         # On donne un point pour chaque mot présent dans le titre.
         score_expr = ' + '.join(['(title LIKE %s)'] * len(words))

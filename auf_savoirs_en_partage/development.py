d06b19d3d79b10867029a7c96dd1919c5d2bf932
Eric Mc Sween <eric.mcsween@gmail.com>
2010-11-01 12:11:10 -0400

    Intégration de la django debug toolbar

--- auf_savoirs_en_partage/development.py
+++ auf_savoirs_en_partage/development.py
@@ -1,3 +1,6 @@
 from auf_savoirs_en_partage.settings import *
 DEBUG=True
 TEMPLATE_DEBUG=DEBUG
+INTERNAL_IPS = ('127.0.0.1',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar',)

40f7947bbefa9a5561820be514e08182fb05f5cb
Eric Mc Sween <eric.mcsween@gmail.com>
2010-11-02 11:47:47 -0400

    Désactiver la django debug toolbar par défaut

--- auf_savoirs_en_partage/development.py
+++ auf_savoirs_en_partage/development.py
@@ -2,6 +2,9 @@ from auf_savoirs_en_partage.settings import *
 DEBUG=True
 TEMPLATE_DEBUG=DEBUG
 INTERNAL_IPS = ('127.0.0.1',)
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-INSTALLED_APPS += ('debug_toolbar',)
-DEBUG_TOOLBAR_CONFIG = dict(INTERCEPT_REDIRECTS=False)
+
+# Activer la debug toolbar...
+
+# MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+# INSTALLED_APPS += ('debug_toolbar',)
+# DEBUG_TOOLBAR_CONFIG = dict(INTERCEPT_REDIRECTS=False)

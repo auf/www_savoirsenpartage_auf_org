3fe754199c2d7c9f60beb204231f887ea280bcde
Eric Mc Sween <eric.mcsween@gmail.com>
2010-11-15 15:00:47 -0500

    Cacher le champ "Autre établissement" lorsqu'un établissement membre est sélectionné.

--- auf_savoirs_en_partage/media/js/chercheur_edit.js
+++ auf_savoirs_en_partage/media/js/chercheur_edit.js
@@ -1,8 +1,22 @@
-$(document).ready(function() {
-    $('#expertises fieldset').formset({
-        prefix: 'expertise',
-        addText: 'ajouter une expertise',
-        deleteText: 'supprimer cette expertise',
-        formCssClass: 'dynamic-form-expertises'
+(function() {
+
+    function update_etablissement_autre() {
+        if ($('#id_chercheur-etablissement').val() == '') {
+            $('#etablissement_autre').slideDown('fast');
+        }
+        else {
+            $('#etablissement_autre').slideUp('fast');
+        }
+    }
+
+    $(document).ready(function() {
+        $('#expertises fieldset').formset({
+            prefix: 'expertise',
+            addText: 'ajouter une expertise',
+            deleteText: 'supprimer cette expertise',
+            formCssClass: 'dynamic-form-expertises'
+        });
+        update_etablissement_autre()
+        $('#id_chercheur-etablissement').change(update_etablissement_autre)
     });
-});
+})();

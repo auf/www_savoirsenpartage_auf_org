fcf1fb09f665461756eeca3f8c52d571a97da28e
Eric Mc Sween <eric.mcsween@gmail.com>
2010-11-15 15:26:07 -0500

    Sur la page d'accueil, afficher les fiches de chercheurs modifiées récemment.
    Demande #727

--- auf_savoirs_en_partage/savoirs/views.py
+++ auf_savoirs_en_partage/savoirs/views.py
@@ -29,7 +29,7 @@ def index(request, discipline=None, region=None):
     actualites = Actualite.objects.filter(visible=True, date__gt=oldest).filter_discipline(discipline).filter_region(region)[:4]
     evenements = Evenement.objects.filter(approuve=True).filter_discipline(discipline).filter_region(region)[:4]
     ressources = Record.objects.filter_discipline(discipline).filter_region(region).random(4)
-    chercheurs = Chercheur.objects.filter_discipline(discipline).filter_region(region).random(10)
+    chercheurs = Chercheur.objects.filter_discipline(discipline).filter_region(region).order_by('-date_modification')[:10]
     sites = Site.objects.filter_discipline(discipline).filter_region(region).random(4)
     return render_to_response(
         "savoirs/index.html",

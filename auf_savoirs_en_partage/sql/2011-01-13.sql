7a882aa42969e93111dc6491fa03b6f3acdb7582
Eric Mc Sween <eric.mcsween@savoirfairelinux.com>
2011-01-13 18:33:55 -0500

    Initialiser correctement la table de migration South

--- auf_savoirs_en_partage/sql/2011-01-13.sql
+++ auf_savoirs_en_partage/sql/2011-01-13.sql
@@ -5,3 +5,7 @@ CREATE TABLE `south_migrationhistory` (
     `applied` datetime NOT NULL
 );
 
+INSERT INTO south_migrationhistory (app_name, migration, applied)
+VALUES ('savoirs', '0001_initial', '2011-01-12 00:00:00'),
+       ('sitotheque', '0001_initial', '2011-01-12 00:00:00'),
+       ('chercheurs', '0001_initial', '2011-01-12 00:00:00');

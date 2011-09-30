b8af106e7569f01342d4db9a74476c74e1a829de
PA Parent <paparent@paparent.me>
2011-09-30 14:10:07 -0400

    Ajout de l'API pour Faun Auteurs

--- auf_savoirs_en_partage/interfaces/views.py
+++ auf_savoirs_en_partage/interfaces/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.http import HttpResponse
+
+from models import FaunAuteur
+
+
+def faun_auteurs(request, id):
+    try:
+        faunauteur = FaunAuteur.objects.get(faun_auteur=id)
+    except FaunAuteur.DoesNotExist:
+        response = HttpResponse()
+    else:
+        response = HttpResponse("http://%s/chercheurs/%d" % (settings.SITE_DOMAIN, faunauteur.sep_chercheur.pk))
+
+    return response

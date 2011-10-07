e4aeae548129f757c8cbf2a5ad37d94b2f50dd1e
PA Parent <paparent@paparent.me>
2011-10-07 15:54:19 -0400

    Rappels: liste seulement chercheur, et ajout permission

--- auf_savoirs_en_partage/rappels/views.py
+++ auf_savoirs_en_partage/rappels/views.py
@@ -7,10 +7,10 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.models import User
 
 
-@permission_required('savoirs.statistiques')
+@permission_required('rappels.rappels')
 def admin_rappels(request):
 
-    chercheurs = User.objects.extra(select={'year': "EXTRACT(year FROM last_login)", 'month': "EXTRACT(month FROM last_login)"}).values('year', 'month').annotate(total=Count('username')).order_by('year', 'month')
+    chercheurs = User.objects.exclude(chercheur__exact=None).extra(select={'year': "EXTRACT(year FROM last_login)", 'month': "EXTRACT(month FROM last_login)"}).values('year', 'month').annotate(total=Count('username')).order_by('year', 'month')
 
     return render_to_response('admin/rappels/rappels.html', {
         'chercheurs': chercheurs

d118529a9dd9feb6d526c42f92ff851679b098b5
PA Parent <paparent@paparent.me>
2011-11-08 15:00:56 -0500

    Interface FAUN: Empêcher les doublons

--- auf_savoirs_en_partage/interfaces/models.py
+++ auf_savoirs_en_partage/interfaces/models.py
@@ -6,7 +6,7 @@ from chercheurs.models import Chercheur
 
 
 class FaunAuteur(models.Model):
-    faun_auteur = models.IntegerField('FAUN Auteur (ID)')
+    faun_auteur = models.IntegerField('FAUN Auteur (ID)', unique=True)
     sep_chercheur = models.ForeignKey(Chercheur, verbose_name='SEP Chercheur')
 
     class Meta:
